47c4f90047898948d151dbddd2e62b89fe5223a3
keshavsoft <nknnkumar@yahoo.com>
2025-02-19 09:49:41 +0530

    websocket index.html popup good

--- FontEnd/ForWebSocket/KeshavSoft/Js/Index/Index.js
+++ FontEnd/ForWebSocket/KeshavSoft/Js/Index/Index.js
@@ -1,11 +1,15 @@
+import { StartFunc as StartFuncFromFormLoad } from "./FormLoad/entryFile.js";
+
 const StartFunc = () => {
+    StartFuncFromFormLoad();
+
     const button = document.getElementById('ShowAllUsersId');
 
     button.addEventListener('click', function () {
         // code to run when button is clicked
         // console.log('button clicked',button);
         webSocket.send("returnOnlineClients");
-        
+
     });
 }
 
