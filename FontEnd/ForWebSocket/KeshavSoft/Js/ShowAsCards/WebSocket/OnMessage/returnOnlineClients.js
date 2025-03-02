5c152472c86905b96a7039dbfec4fa0ac25a9d05
KarthikMalasani <karthikmalasani21@gmail.com>
2025-03-02 14:35:14 +0530

    Updated Html Cards and JS

--- FontEnd/ForWebSocket/KeshavSoft/Js/ShowAsCards/WebSocket/OnMessage/returnOnlineClients.js
+++ FontEnd/ForWebSocket/KeshavSoft/Js/ShowAsCards/WebSocket/OnMessage/returnOnlineClients.js
@@ -3,7 +3,6 @@ let StartFunc = ({ inUsersAsArray }) => {
 
     inUsersAsArray.forEach(element => {
         jFInsertCard({ inCardData: element });
-        // jFInsertRoundIcons({ inCardData: element });
     });
 };
 
@@ -15,32 +14,13 @@ const jFInsertCard = ({ inCardData }) => {
         const template = document.querySelector("#TemplateForCard");
 
         const clone = template.content.cloneNode(true);
-        let td = clone.querySelectorAll("h6");
-        td[0].textContent = inCardData.Name;
+
+        let subItemTitle = clone.querySelector(".sub-item-title");
+        subItemTitle.textContent = inCardData.Name;
+
 
         let jVarLocalTimeSpan = clone.querySelector(".TimeSpanClass");
         jVarLocalTimeSpan.textContent = inCardData.loginDateTime;
-
-        jVarLocalToInsertHtml.appendChild(clone);
-    } else {
-        // Find another way to add the rows to the table because
-        // the HTML template element is not supported.
-    };
-};
-
-const jFInsertRoundIcons = ({ inCardData }) => {
-    if ("content" in document.createElement("template")) {
-        const jVarLocalToInsertHtml = document.querySelector("#RoundIconsContainerId");
-        jVarLocalToInsertHtml.innerHTML = "";
-
-        const template = document.querySelector("#TemplateForRoundIcon");
-
-        const clone = template.content.cloneNode(true);
-        let td = clone.querySelector("p");
-        td.textContent = inCardData.Name;
-
-        // let jVarLocalTimeSpan = clone.querySelector(".TimeSpanClass");
-        // jVarLocalTimeSpan.textContent = inCardData.loginDateTime;
 
         jVarLocalToInsertHtml.appendChild(clone);
     } else {
